b7ab5e7df1fffdeab0bc9fe395b3481d5d7d3180
barazd <barazd@gmail.com>
2025-04-21 20:40:23 +0200

    add: táblázat alapok + webstorage osztály, mert máshova is kelleni fog

---
+++ feladat_1/tablazat.js
@@ -0,0 +1,27 @@
+import { webStorage } from "./webStorage";
+
+// Étterem típusok
+const restaurantTypes = [
+    { id: 1, title: 'Pizza' },
+    { id: 2, title: 'Kínai' },
+    { id: 3, title: 'Olasz' },
+    { id: 4, title: 'Magyaros' },
+    { id: 5, title: 'Gyros' },
+    { id: 6, title: 'Indiai' },
+    { id: 7, title: 'Vietnámi' },
+    { id: 8, title: 'Hamburger' },
+    { id: 9, title: 'Vegán' },
+    { id: 10, title: 'Cukrászda' },
+];
+
+// Kezdő adatok
+const initialData = [
+    { id: crypto.randomUUID(), name: 'Tattoria la prima', city: 'Eger', type: 1, rating: 8 },
+    { id: crypto.randomUUID(), name: 'Nyolckincs Étterem', city: 'Budapest IX.', type: 2, rating: 6 },
+    { id: crypto.randomUUID(), name: 'Sugo de Italia', city: 'Kecskemét', type: 3, rating: 10 },
+    { id: crypto.randomUUID(), name: 'Öregponty Csárda', city: 'Szeged', type: 4, rating: 4 },
+    { id: crypto.randomUUID(), name: 'Pasha Dürüm', city: 'Győr', type: 5, rating: 7 },
+    { id: crypto.randomUUID(), name: 'Jal Mahal Falatozó', city: 'Budapest III.', type: 6, rating: 8 },
+];
+
+const store = new webStorage('webprog1-ea', initialData);
---
+++ feladat_1/webStorage.js
@@ -0,0 +1,48 @@
+export class webStorage {
+    constructor(storeName, initialData) {
+        this.storeName = storeName;
+
+        // Ha nem létezik létrehozzuk
+        if (!localStorage.getItem(this.storeName)) {
+            this.saveItems(initialData);
+        }
+    }
+
+    // Segéd függvények
+
+    findById(items, id) {
+        return items.find((item) => item.id === id);
+    }
+
+    saveItems(items) {
+        localStorage.setItem(this.storeName, items);
+    }
+
+    // CRUD + index
+
+    getItems() {
+        return JSON.parse(localStorage.getItem(this.storeName)).sort((a, b) => b.id - a.id);
+    }
+
+    readItem(id) {
+        return this.findById(JSON.parse(localStorage.getItem(this.storeName)), id);
+    }
+
+    createItem(data) {
+        // Ha nincs id, csinálunk
+        if (data.id === undefined) {
+            data.id = crypto.randomUUID();
+        }
+        let items = getItems();
+        items.push(data);
+        saveItems(items);
+    }
+
+    updateItem(id, data) {
+
+    }
+
+    deleteItem(id) {
+
+    }
+}
