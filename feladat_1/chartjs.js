d616459680c10dabd074e13c1aaa86f749d587ec
barazd <barazd@gmail.com>
2025-04-26 23:24:17 +0200

    add: formázás

--- feladat_1/chartjs.js
+++ feladat_1/chartjs.js
@@ -2,8 +2,6 @@ import { Chart, registerables } from 'https://cdn.jsdelivr.net/npm/chart.js@4.4.
 
 Chart.register(...registerables);
 
-const ctx = document.getElementById('chart');
-
 // Random adatsor generálók
 function getRandomDataset(length = 5, min = 5, max = 50) {
     return Array.from({ length }, () => Math.floor(Math.random() * (max - min) + min));
@@ -18,7 +16,27 @@ function getRandomLabel() {
 // Adatok mennyisége
 const length = 5;
 
-new Chart(ctx, {
+// Táblázat
+const table = document.querySelector('#table');
+
+// Ha kattintunk a jelmagyarázaton, akkor a checkbox-ok kövessék azt
+const handleChartClick = (event, legendItem, legend) => {
+    // A checkbox állítása
+    Array.from(table.querySelectorAll('input[type=checkbox]')).forEach((el) => {
+        if (el.value == legendItem.datasetIndex) {
+            el.checked = legendItem.hidden;
+        }
+    });
+    // Csinálja meg, amit eredetileg csinálna
+    legend.chart.data.datasets[legendItem.datasetIndex].hidden = !legendItem.hidden;
+    legend.chart.update();
+}
+
+// Chart.js
+
+const ctx = document.getElementById('chart');
+
+const cfg = {
     type: 'line',
     data: {
         labels: getRandomDataset(length).sort((a, b) => a - b),
@@ -26,7 +44,9 @@ new Chart(ctx, {
             data: getRandomDataset(),
             label: 'L' + getRandomColor().slice(-2).toUpperCase(),
             borderColor: getRandomColor(),
-            fill: false
+            tension: 0.4,
+            fill: false,
+            hidden: false,
         }))
     },
     options: {
@@ -38,6 +58,37 @@ new Chart(ctx, {
             y: {
                 beginAtZero: true,
             }
+        },
+        plugins: {
+            legend: {
+                onClick: handleChartClick
+            }
         }
     }
+};
+
+const chart = new Chart(ctx, cfg);
+
+// Adatokat tartalmazó táblázat
+
+table.querySelector('thead').innerHTML = '<tr>' + 
+    '<th></th><th>Adatsor</th>' +
+    cfg.data.labels.reduce((row, label) => row + 
+        `<th>${label}</th>`, '') + 
+    '</tr>';
+
+    table.querySelector('tbody').innerHTML = cfg.data.datasets.reduce((html, dataset, index) => html + '<tr>' + 
+        `<td style="background:${dataset.borderColor}"><input type="checkbox" value="${index}" ${dataset.hidden ? '' : 'checked'}></td>` + 
+        `<td><strong>${dataset.label}</strong></td>` +
+        dataset.data.reduce((row, item) => row + 
+        `<td>${item}</td>`, '') + 
+    '</tr>', '');
+
+// Eseményfigyelő ha a checkbox-okat változtatjuk
+Array.from(table.querySelectorAll('input[type=checkbox]')).forEach((el) => {
+    el.addEventListener('change', (event) => {
+        // A checkbox-nak megfelelően frissítjük a chart-ot
+        chart.data.datasets[event.target.value].hidden = !event.target.checked;
+        chart.update();
+    });
 });
