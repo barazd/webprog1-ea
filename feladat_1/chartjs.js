788f454f13562d98ab37bf1f77e918e4581c4d91
barazd <barazd@gmail.com>
2025-04-26 21:55:35 +0200

    add: js migrálása külön fájlba

---
+++ feladat_1/chartjs.js
@@ -0,0 +1,24 @@
+import { Chart, registerables } from 'https://cdn.jsdelivr.net/npm/chart.js@4.4.9/+esm';
+
+Chart.register(...registerables);
+
+const ctx = document.getElementById('chart');
+
+new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        datasets: [{
+            label: '# of Votes',
+            data: [12, 19, 3, 5, 2, 3],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
+    }
+});
